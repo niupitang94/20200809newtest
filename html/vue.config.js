e74e5c2c22506b0f1496234302069b64731f60cb
梅杰 <1292094506@qq.com>
2020-08-09 15:42:10 +0800

    一个vue的脚手架

---
+++ html/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/production-sub-path/" : "/",
+    devServer: {
+      proxy: {
+        '/api': {}
+      } //请求的API指向这个路径
+    }
+  };
